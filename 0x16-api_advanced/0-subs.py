f8027d7c13502066aa2a11f39a247035b0f3bc7c
root <root@stan>
2023-11-07 23:23:17 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+""" gets the number of subs for a subredit """
+import json
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'custom'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
+    """function that takes a subredit and returns number of subs"""
+    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+    data = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
         return 0
-
-    data = response.json()
-    return data['data']['subscribers']
+    json_data = data.json()
+    results = json_data.get("data").get("subscribers")
+    return results
